017b2587adf5882093c1bdc6a362e4cd314673c9
Kaskyjames <izuchukwujameskasky@gmail.com>
2025-06-23 12:43:42 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function openLightbox(imageUrl) {
+  const lightbox = document.getElementById("lightbox");
+  const img = document.getElementById("lightbox-img");
+  img.src = imageUrl;
+  lightbox.style.display = "flex";
+}
+
+document.addEventListener("click", function (e) {
+  if (e.target.id === "lightbox") {
+    document.getElementById("lightbox").style.display = "none";
+  }
+});
